a6890af0ed9281c25d6921d6800be8363b3214fa
KaraGOD <gasdiscord@gmail.com>
2024-11-15 22:35:33 +0500

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,411 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import "package:path/path.dart" show dirname;
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Axle4AlgoTgUser',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class AppConfig {
+  String login;
+  String phone;
+  String text;
+  String agent;
+  String agentPass;
+  String passwordTg;
+  bool closed;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  AppConfig(
+      {required this.login,
+        required this.phone,
+        required this.text,
+        required this.agent,
+        required this.agentPass,
+        required this.passwordTg,
+        this.closed = false});
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  factory AppConfig.fromMap(Map<String, dynamic> map) {
+    return AppConfig(
+        login: map['login'] as String,
+        phone: map['phone'] as String,
+        text: map['text'] as String,
+        agent: map['agent'] as String,
+        agentPass: map['agentPass'] as String,
+        passwordTg: map['passwordTg'] as String
+    );
+  }
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Map<String, dynamic> toMap() {
+    return {
+      'login': login,
+      'phone': phone,
+      'text': text,
+      'agent': agent,
+      'agentPass': agentPass,
+      'passwordTg': passwordTg
+    };
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
-  void _incrementCounter() {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  List<AppConfig> _appConfigs = [];
+  List<Process?> _processes = [];
+  List<TextEditingController> _outputControllers = [];
+  List<TextEditingController> _inputControllers = [];
+  TabController? _tabController;
+  final agentController = TextEditingController();
+  final agentPassController = TextEditingController();
+
+  final List<String> highlightTriggers = [
+    'Введи ПОРЯДКОВЫЙ номер кнопки',
+    'Доступных кнопок нет',
+    'ERROR',
+    'Не удалось',
+    'message.entities[0].user IS None',
+    'пользователь без логина, id нет',
+    'НЕ найдено фото, фото НЕ скачано',
+    'ДОСТИГНКТ ЛИМИТ',
+    'ACCESS DENIED',
+    'ЧЕЛОВЕК НЕ ДОБАВЛЕН В ИГНОР ЛИСТ',
+    'ОШИБКА ОТПРАВКИ',
+    'CONNECTION TERMINATED',
+    'нет логина или id',
+    'Ошибка',
+    'Вы забыли заполнить конфиг'
+  ];
+  List<bool> _isTabHighlighted = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAppConfigs();
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    for (var controller in _outputControllers) {
+      controller.dispose();
+    }
+    for (var controller in _inputControllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  Future<void> _loadAppConfigs() async {
+    final prefs = await SharedPreferences.getInstance();
+    //prefs.clear();
+    final String? jsonString = prefs.getString('app_configs');
+    if (jsonString != null) {
+      List<dynamic> jsonList = jsonDecode(jsonString);
+      setState(() {
+        _appConfigs = jsonList.map((json) => AppConfig.fromMap(json)).toList();
+        _outputControllers = List.generate(_appConfigs.length, (_) => TextEditingController(), growable:true);
+        _inputControllers = List.generate(_appConfigs.length, (_) => TextEditingController(), growable:true);
+        _isTabHighlighted = List.filled(_appConfigs.length, false, growable:true);
+        if (_appConfigs.isNotEmpty) {
+          agentController.text = _appConfigs[0].agent;
+          agentPassController.text = _appConfigs[0].agentPass;
+        }
+      });
+      _initializeTabController();
+      _startAllProcesses();
+    }
+  }
+
+  void _initializeTabController() {
+    _tabController?.dispose();
+    if (_appConfigs.isNotEmpty) {
+      _tabController = TabController(length: _appConfigs.length, vsync: this);
+    } else {
+      _tabController = null;
+    }
+  }
+
+  Future<void> _saveAppConfigs() async {
+    final prefs = await SharedPreferences.getInstance();
+    //prefs.clear();
+    String? jsonString = prefs.getString('app_configs');
+    jsonString = jsonEncode(_appConfigs.map((config) => config.toMap()).toList());
+    await prefs.setString('app_configs', jsonString);
+  }
+
+  Future<void> _startAllProcesses() async {
+    for (int i = 0; i < _appConfigs.length; i++) {
+      _startProcess(i);
+    }
+  }
+
+  Future<void> _startProcess(int index) async {
+    final config = _appConfigs[index];
+    try {
+      final process = await Process.start(
+          "${dirname(Platform.resolvedExecutable.toString())}\\src\\AlgoTgUser.exe",
+          ['--login=${config.login}', '--phone=${config.phone}',
+            '--text=${config.text}', '--agent=${config.agent}',
+            '--agentPass=${config.agentPass}', '--passwordTg=${config.passwordTg}']);
+      _processes.add(process);
+
+      process.stdout.transform(utf8.decoder).listen((data) {
+        setState(() {
+          _outputControllers[index].text += data;
+        });
+        _checkForTriggers(data, index);
+      });
+
+      process.stderr.transform(utf8.decoder).listen((data) {
+        setState(() {
+          _outputControllers[index].text += 'ERROR: $data';
+        });
+        _checkForTriggers(data, index);
+      });
+
+      setState(() {});
+    } catch (e) {
+      setState(() {
+        _outputControllers[index].text += 'Error starting process: $e';
+      });
+    }
+  }
+
+  void _checkForTriggers(String data, int index) {
+    for (var trigger in highlightTriggers) {
+      if (data.toLowerCase().contains(trigger.toLowerCase())) {
+        setState(() {
+          _isTabHighlighted[index] = true;
+          _tabController?.animateTo(index);
+        });
+        break;
+      }
+    }
+  }
+
+  void _sendInput(int index) {
+    final input = _inputControllers[index].text;
+    _processes[index]?.stdin.writeln(input);
+    _inputControllers[index].clear();
+    _isTabHighlighted[index] = false;
+  }
+
+  void _addAppConfig(String login, String phone, String text, String agent,
+      String agentPass, String passwordTg) {
+    final newConfig = AppConfig(login: login, phone: phone,
+        text: text, agent: agent, agentPass:agentPass, passwordTg:passwordTg);
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _appConfigs.add(newConfig);
+      _outputControllers.add(TextEditingController());
+      _inputControllers.add(TextEditingController());
+      _isTabHighlighted.add(false);
+      _initializeTabController();
     });
+    _saveAppConfigs();
+    _startProcess(_appConfigs.length - 1);
+  }
+
+  void _removeAppConfig(int index, {bool save = true}) {
+    setState(() {
+      _appConfigs[index].closed = true;
+      if (save){
+        _appConfigs.removeAt(index);
+      }
+      _processes[index]?.kill();
+      _processes.removeAt(index);
+      _outputControllers.removeAt(index);
+      _inputControllers.removeAt(index);
+      _isTabHighlighted.removeAt(index);
+      _initializeTabController();
+    });
+    if (save){
+      _saveAppConfigs();
+    }
+  }
+
+  void _openSettingsDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Настройки менеджера TgUser'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: agentController,
+                  decoration: const InputDecoration(labelText: 'ВАШ логин ОСНОВНОГО тг без "@"'),
+                  readOnly: (agentController.text.isNotEmpty) ? true:false),
+              TextField(controller: agentPassController,
+                  decoration: const InputDecoration(labelText: 'ВАШ пароль агента'),
+                  readOnly: (agentPassController.text.isNotEmpty) ? true:false) ,
+              ...List.generate(_appConfigs.length, (index) {
+                return ListTile(
+                  title: Text('${_appConfigs[index].login} ${index + 1}: ${_appConfigs[index].phone}'),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      _removeAppConfig(index);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                );
+              }, growable: true),
+              const Divider(),
+              ElevatedButton(
+                onPressed: () {
+                  final loginController = TextEditingController();
+                  final phoneController = TextEditingController();
+                  final textController = TextEditingController();
+                  final passwordTgController = TextEditingController();
+
+                  showDialog(
+                    context: context,
+                    builder: (_) => AlertDialog(
+                      title: const Text('Добавить новую конфигурацию'),
+                      content: SingleChildScrollView(
+                        child: Column(
+                          children: [
+                            TextField(controller: loginController, decoration: const InputDecoration(labelText: 'Логин тг без "@"')),
+                            TextField(controller: phoneController, decoration: const InputDecoration(labelText: 'Номер телефона в международном формате(+7...)')),
+                            TextField(controller: textController, decoration: const InputDecoration(labelText: 'Спам текст')),
+                            TextField(controller: passwordTgController, decoration: const InputDecoration(labelText: 'Пароль двухфакторной аутентификации\n(оставьте поле пустым если нет)'))
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            _addAppConfig(
+                                loginController.text,
+                                phoneController.text,
+                                textController.text,
+                                agentController.text,
+                                agentPassController.text,
+                                (passwordTgController.text.isNotEmpty) ? passwordTgController.text : "0"
+                            );
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Добавить'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: const Text('Отмена'),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: const Text('Добавить новую конфигурацию'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Закрыть'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Axle4AlgoTgUser (${_processes.length} конфигураций запущенно)"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: _openSettingsDialog,
+          ),
+        ],
+        bottom: _tabController != null
+            ? TabBar(
+          isScrollable: true,
+          controller: _tabController,
+          tabs: List<Widget>.generate(
+              _appConfigs.length,
+                  (index) => _appConfigs[index].closed ? Container()
+                  :Tab(
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          color: _isTabHighlighted[index] ? Colors.red : Colors.transparent,
+                          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                          child: Text('${_appConfigs[index].login} ${index + 1}'),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.close, size: 18),
+                          onPressed: () => _removeAppConfig(index, save: false), // Call _closeTab when pressed
+                        )
+                      ]
+                )
+              ), growable: true),
+        )
+            : null,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: _tabController != null
+          ? TabBarView(
+        controller: _tabController,
+        children: List<Widget>.generate(_appConfigs.length, (index) {
+          if (_appConfigs[index].closed){
+            return Container();
+          }
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _outputControllers[index],
+                    style: const TextStyle(fontFamily: 'Courier'),
+                    maxLines: null,
+                    readOnly: true,
+                    decoration: const InputDecoration(
+                      isDense: false,
+                      labelText: 'Вывод',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                TextField(
+                  controller: _inputControllers[index],
+                  decoration: const InputDecoration(
+                    labelText: 'Ввод',
+                    border: OutlineInputBorder(),
+                  ),
+                  onSubmitted: (_) => _sendInput(index),
+                ),
+                ElevatedButton(
+                  onPressed: () => _sendInput(index),
+                  child: const Text('Отправить'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          );
+        }, growable: true),
+      )
+          : const Center(child: Text('Нет конфигураций')),
     );
   }
 }
